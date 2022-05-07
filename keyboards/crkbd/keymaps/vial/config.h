f7e070a1f978887f1751aab1628a86f74a732910
mkissarli <kissarlim@gmail.com>
2022-05-07 11:01:30 +0100

    Add working unlock buttons for macros

--- keyboards/crkbd/keymaps/vial/config.h
+++ keyboards/crkbd/keymaps/vial/config.h
@@ -18,8 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
-#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+#define VIAL_UNLOCK_COMBO_ROWS {2, 2}
+#define VIAL_UNLOCK_COMBO_COLS {2, 3}
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #define TAPPING_TERM 180
